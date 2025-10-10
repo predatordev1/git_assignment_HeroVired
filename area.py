82e09ae5d47f342740275b2676a8d10abfd04f55
Devendra Kumar <devendra8182@gmail.com>
2025-10-10 15:58:05 +0530

    hi

--- area.py
+++ area.py
@@ -11,15 +11,7 @@ if __name__ == "__main__":
 
 radius = float(input("Enter the radius of circle: "))
 print(f"The area of the circle with radius {radius} ={calculator.calculate_circle_area(radius)}")
-<<<<<<< HEAD
-
-=======
->>>>>>> 9e69b1e (bug fixed)
  
 length = float(input("Enter the length of rectangle: "))
 width = float(input("Enter the width of rectangle: "))
 print(f"The area of the rectangle with length {length} and width {width} = {calculator.calculate_rectangle_area(length, width)}")
-<<<<<<< HEAD
-
-=======
->>>>>>> 9e69b1e (bug fixed)
