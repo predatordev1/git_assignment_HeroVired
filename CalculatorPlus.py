937c64036269aa8b1e8a7fec8ce82f0480132ba8
RiteshGIT-Lab <RiteshITAarav@gmail.com>
2025-10-10 12:50:14 +0530

    Update CalculatorPlus.py

--- CalculatorPlus.py
+++ CalculatorPlus.py
@@ -8,7 +8,8 @@ class Calculators:
         def multiply(self, a, b):
                 return a * b
         def divide(self, a, b):
-                if b == 0
+                if b == 0:
+return "cannot divide by Zero."
                 return a / b
         def square_root(self, x):
                 return math.sqrt(x)
