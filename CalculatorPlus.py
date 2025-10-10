c6a8772ca31e5555ebe4d728999560358e5f2fbd
Devendra Kumar <devendra8182@gmail.com>
2025-10-10 13:00:13 +0530

    Version 2

--- CalculatorPlus.py
+++ CalculatorPlus.py
@@ -8,8 +8,6 @@ class Calculators:
         def multiply(self, a, b):
                 return a * b
         def divide(self, a, b):
-                if b == 0:
-return "cannot divide by Zero."
                 return a / b
         def square_root(self, x):
                 return math.sqrt(x)
@@ -17,14 +15,13 @@ return "cannot divide by Zero."
 if __name__ == "__main__":
         calculator = Calculators()
 
-num1 = 16
-num2 = 4
+num1 = float(input("Enter the first Number :"))
+num2 = float(input("Enter the Second Number :"))
 
 print(f"{num1} + {num2} = {calculator.add(num1, num2)}")
 print(f"{num1} - {num2} = {calculator.subtract(num1, num2)}") 
 print(f"{num1} * {num2} = {calculator.multiply(num1, num2)}")
 print(f"{num1} / {num2} = {calculator.divide(num1, num2)}")
 
-num3 = 25
+num3 = float(input("Enter the square root Number :"))
 print(f"The square root of {num3} = {calculator.square_root(num3)}")
-
