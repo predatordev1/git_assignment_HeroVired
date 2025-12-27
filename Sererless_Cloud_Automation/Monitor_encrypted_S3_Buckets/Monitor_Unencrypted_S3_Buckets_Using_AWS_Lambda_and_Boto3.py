a9466ce743a5055c178e7fea61196ce808463c57
Devendra Kumar <157613023+predatordev1@users.noreply.github.com>
2025-12-27 18:20:01 +0530

    Add files via upload

---
+++ Sererless_Cloud_Automation/Monitor_encrypted_S3_Buckets/Monitor_Unencrypted_S3_Buckets_Using_AWS_Lambda_and_Boto3.py
@@ -0,0 +1,35 @@
+import boto3
+
+def lambda_handler(event, context):
+    s3_client = boto3.client("s3")
+    response = s3_client.list_buckets()
+    sse_s3_buckets = []
+    sse_kms_buckets = []
+    dsse_kms_buckets = []
+
+    for bucket in response['Buckets']:
+        try:
+            bucket_name = bucket['Name']
+            encryption_check = s3_client.get_bucket_encryption(Bucket=bucket_name)
+            sseAlgorithm = encryption_check['ServerSideEncryptionConfiguration']['Rules'][0]['ApplyServerSideEncryptionByDefault']['SSEAlgorithm']
+            if sseAlgorithm == 'AES256':
+                sse_s3_buckets.append(bucket_name)
+            elif sseAlgorithm == 'aws:kms':
+                sse_kms_buckets.append(bucket_name)
+            else:
+                dsse_kms_buckets.append(bucket_name)
+            
+        except s3_client.exceptions.ServerSideEncryptionConfigurationNotFoundError:
+            print(f"✗ {bucket_name}: NO ENCRYPTION (rare)")
+        except Exception as e:
+            print(f"⚠ {bucket_name}: Error - {e}")
+
+    print(f"\n{'*'*50}")
+    print(f"\nSSE-S3 buckets: {sse_s3_buckets}")
+    print(f"\nSSE-KMS buckets: {sse_kms_buckets}")
+    print(f"\nDSSE-KMS buckets: {dsse_kms_buckets}\n")
+    print(f"\n{'*'*50}")
+    return{
+        'statusCode': 200,
+        'body': f'{len(sse_s3_buckets)} buckets are with SSE-S3 encryption named {sse_s3_buckets}.\n {len(sse_kms_buckets)} buckets are with SSE-KMS encryption named {sse_kms_buckets}.\n{len(dsse_kms_buckets)} buckets are with DSSE-KMS encryption named {dsse_kms_buckets}'
+        }
