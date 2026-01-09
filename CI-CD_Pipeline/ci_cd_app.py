7b8b0e39a466c361e1155bfef78e8f642f7f41be
Super User <root@localhost.localdomain>
2026-01-10 00:32:00 +0530

    added new contact in flask app

--- CI-CD_Pipeline/ci_cd_app.py
+++ CI-CD_Pipeline/ci_cd_app.py
@@ -2,9 +2,13 @@ from flask import Flask, jsonify, request
 
 ci_cd_app = Flask(__name__)
 
-@ci_cd_app.route("/contact", methods = ["POST", "GET"])
+@ci_cd_app.route("/", methods=["GET"])
+def health_check():
+    return "Flask app is running!"
+
+@ci_cd_app.route("/contact", methods=["POST", "GET"])
 def Hello():
-    return("Hello Devendra")
+    return "Hello Devendra"
 
 if __name__ == '__main__':
     ci_cd_app.run(host='0.0.0.0', port=5000, debug=False)
